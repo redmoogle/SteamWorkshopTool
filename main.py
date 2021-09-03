bea3318e32b46335354f97156161f15876cced14
Redmoogle <dakotamew@gmail.com>
2021-09-03 19:14:13 -0400

    MUCH better UI and some features

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import subprocess
 import shutil
 
 import tkinter as tk
-from tkinter.filedialog import askdirectory
+from tkinter.filedialog import askdirectory, askopenfile
 
 from modules import Steamcmd, SteamcmdException
 import vdf
@@ -28,6 +28,13 @@ class GUI(tk.Tk):
         self.vdf = None    # Workshop Data
         self.parsed = None # Full VDF Data
 
+        # Paths
+        self.modent = None
+        self.imgent = None
+
+        self.modfile = ''
+        self.imgfile = ''
+
         # Steamcmd
         self.scmd = None
 
@@ -37,17 +44,17 @@ class GUI(tk.Tk):
         self.auth = tk.StringVar()
 
         # Window Stuff
-        self['bg'] = '#5f7b4d'
+        self['bg'] = '#637e94'
         self.title('Steam Workshop Utility')
 
         self.gui_MainMenu()
 
     def gui_MainMenu(self):
         self.clear_frame()
-        self.grn_btn(self, text="New Mod", command=self.gui_New).grid(row=0)
-        self.grn_btn(self, text="Load Mod", command=self.load).grid(row=1)
-        self.grn_btn(self, text="Exit", command=self.close).grid(row=2)
-        self.geometry('100x120')
+        self.grn_btn(self, text="New Mod", width=10, command=self.gui_New).grid(row=0)
+        self.grn_btn(self, text="Load Mod", width=10, command=self.load).grid(row=1)
+        self.grn_btn(self, text="Exit", width=10, command=self.close).grid(row=2)
+        self.geometry('112x93')
 
     def gui_vdfEditor(self):
         self.clear_frame()
@@ -65,27 +72,38 @@ class GUI(tk.Tk):
             visval = visibility_rev[selection]
             self.parsed['visibility'] = visval
 
-        # Workshop Desc
-        tk.Label(text='Workshop Description', bg='#5f7b4d').place(x=10, y=10)
-        modDesc = tk.Text(width=50, height=39)
+        def saveimg():
+            self.imgfile = askopenfile(filetypes=[('JPEG Files', '.jpg')]).name
+            self.imgetn.insert(0, self.imgfile)
 
-        tk.Label(text='App-ID', bg='#5f7b4d').place(x=450, y=10)
+        def savemod():
+            self.modfile = askopenfile(filetypes=[('Mod File', '*')]).name
+            self.modetn.insert(0, self.modfile)
+
+        # Workshop Desc
+        tk.Label(text='Workshop Description', bg='#637e94').place(x=5, y=150)
+        modDesc = tk.Text(width=78, height=15)
+
+        tk.Label(text='App-ID', bg='#637e94').place(x=5, y=10)
         appID = tk.Entry()
 
-        tk.Label(text='File ID', bg='#5f7b4d').place(x=450, y=55)
+        tk.Label(text='File ID', bg='#637e94').place(x=5, y=70)
         fileID = tk.Entry()
 
-        tk.Label(text='Title', bg='#5f7b4d').place(x=450, y=100)
+        tk.Label(text='Title', bg='#637e94').place(x=250, y=10)
         modTitle = tk.Entry(width=30)
 
-        tk.Label(text='Visibility', bg='#5f7b4d').place(x=450, y=145)
+        tk.Label(text='Visibility', bg='#637e94').place(x=250, y=70)
         visibilityMenu = tk.OptionMenu(self, tk.StringVar(value=visval), *visibility_rev.keys(), command=upd_visval)
 
-        tk.Label(text='Image Path', bg='#5f7b4d').place(x=450, y=200)
+        tk.Label(text='Image Path', bg='#637e94').place(x=490, y=10)
         previewPath = tk.Entry(width=30)
 
-        tk.Label(text='Mod Path', bg='#5f7b4d').place(x=450, y=250)
+        tk.Label(text='Mod Path', bg='#637e94').place(x=490, y=70)
         modPath = tk.Entry(width=30)
+
+        self.imgetn = previewPath
+        self.modetn = modPath
 
         self.data['description'] = modDesc
         self.data['appid'] = appID
@@ -96,8 +114,8 @@ class GUI(tk.Tk):
 
         for key in self.data:
             obj = self.data[key]
-            obj['bg'] = '#85a76f'
-            obj['highlightbackground'] = '#86c45e'
+            obj['bg'] = '#ccdae6'
+            obj['highlightbackground'] = '#7598b6'
 
         modDesc.insert('end', self.parsed['description'])
         appID.insert(0, self.parsed['appid'])
@@ -106,15 +124,21 @@ class GUI(tk.Tk):
         previewPath.insert(0, self.parsed['previewfile'])
         modPath.insert(0, self.parsed['contentfolder'])
 
-        visibilityMenu.config(width=20, bg='#5f7b4d', highlightbackground='#86c45e')
+        visibilityMenu.config(width=24, bg='#ccdae6', highlightbackground='#7598b6')
 
-        modDesc.place(x=10, y=30)
-        appID.place(x=450, y=30)
-        fileID.place(x=450, y=75)
-        modTitle.place(x=450, y=120)
-        visibilityMenu.place(x=450, y=165)
-        previewPath.place(x=450, y=225)
-        modPath.place(x=450, y=270)
+
+        self.imgbtn = self.grn_btn(self, text='Browse', width=3, command=saveimg)
+        self.modbtn = self.grn_btn(self, text='Browse', width=3, command=savemod)
+        self.imgbtn.place(x=490, y=35)
+        self.modbtn.place(x=490, y=95)
+
+        modDesc.place(x=5, y=170)
+        appID.place(x=5, y=35, height=30, width=220)
+        fileID.place(x=5, y=95, height=30, width=220)
+        modTitle.place(x=250, y=35, height=30, width=220)
+        visibilityMenu.place(x=250, y=95, height=30, width=220)
+        previewPath.place(x=545, y=35, height=30, width=220)
+        modPath.place(x=545, y=95, height=30, width=220)
 
         def save():
             for dataobj in self.data:
@@ -134,22 +158,22 @@ class GUI(tk.Tk):
             save()
             self.gui_Upload()
 
-        backb = self.grn_btn(self, text="Back", command=self.gui_MainMenu)
-        saveb = self.grn_btn(self, text="Save", command=save)
-        uplb = self.grn_btn(self, text="Upload", command=savenup)
+        backb = self.grn_btn(self, text="Back", width=6, command=self.gui_MainMenu)
+        saveb = self.grn_btn(self, text="Save", width=6,command=save)
+        uplb = self.grn_btn(self, text="Upload", width=15, command=savenup)
 
-        backb.pack(side=tk.BOTTOM, anchor=tk.E)
-        saveb.pack(side=tk.BOTTOM, anchor=tk.E)
-        uplb.pack(side=tk.BOTTOM, anchor=tk.E)
+        backb.place(x=636, y=400)
+        saveb.place(x=708, y=400)
+        uplb.place(x=636, y=370)
 
-        self.geometry('800x700')
+        self.geometry('790x435')
 
     def gui_New(self):
         self.clear_frame()
 
-        tk.Label(self, text='Mod Name', bg='#5f7b4d').grid(row=0,column=0)
-        self.modname = tk.Entry(highlightbackground='#86c45e', bg='#85a76f')
-        self.modname.grid(row=0,column=1)
+        tk.Label(self, text='Mod Name', bg='#637e94').place(x=5, y=5)
+        self.modname = tk.Entry(highlightbackground='#7598b6', bg='#ccdae6')
+        self.modname.place(x=90, y=5)
 
         def create():
             _moddir = self.modname.get().strip(' ')
@@ -158,8 +182,8 @@ class GUI(tk.Tk):
             self.vdfloc = f'{_moddir}/{_moddir}.vdf'
             self.gui_vdfEditor()
 
-        self.grn_btn(self, text="Create", command=create).place(x=10, y=35)
-        self.grn_btn(command=self.gui_vdfEditor, text='Back').place(x=85, y=35)
+        self.grn_btn(self, text="Create", command=create, width=12).place(x=5, y=35)
+        self.grn_btn(command=self.gui_MainMenu, text='Back', width=12).place(x=128, y=35)
         self.geometry('260x80')
 
     def gui_Upload(self):
@@ -170,15 +194,15 @@ class GUI(tk.Tk):
         except SteamcmdException:
             print('SteamCMD Already Installed')
 
-        tk.Label(self, text='Username', bg='#5f7b4d').grid(row=0)
-        tk.Label(self, text='Password', bg='#5f7b4d').grid(row=1)
-        tk.Label(self, text='Auth Code', bg='#5f7b4d').grid(row=2)
-        tk.Entry(textvariable=self.usr, bg='#85a76f', highlightbackground='#86c45e').grid(row=0, column=1)
-        tk.Entry(textvariable=self.pwd, show=False, bg='#85a76f', highlightbackground='#86c45e').grid(row=1, column=1)
-        tk.Entry(textvariable=self.auth, bg='#85a76f', highlightbackground='#86c45e').grid(row=2, column=1)
-        self.grn_btn(command=self.finish, text='Upload').grid(row=3)
-        self.grn_btn(command=self.gui_vdfEditor, text='Back').grid(row=3, column=1)
-        self.geometry('200x120')
+        tk.Label(self, text='Username', bg='#637e94').place(x=5, y=5)
+        tk.Label(self, text='Password', bg='#637e94').place(x=5, y=30)
+        tk.Label(self, text='Auth Code', bg='#637e94').place(x=5, y=55)
+        tk.Entry(textvariable=self.usr, bg='#ccdae6', highlightbackground='#7598b6').place(x=80, y=5)
+        tk.Entry(textvariable=self.pwd, show=False, bg='#ccdae6', highlightbackground='#7598b6').place(x=80, y=30)
+        tk.Entry(textvariable=self.auth, bg='#ccdae6', highlightbackground='#7598b6').place(x=80, y=55)
+        self.grn_btn(command=self.finish, text='Upload', width=15).place(x=5, y=80)
+        self.grn_btn(command=self.gui_vdfEditor, text='Back', width=8).place(x=150, y=80)
+        self.geometry('255x120')
 
     def finish(self):
         subprocess.run([
@@ -210,7 +234,7 @@ class GUI(tk.Tk):
         self.gui_vdfEditor()
 
     def grn_btn(self, *args, **kwargs):
-        return tk.Button(*args, **kwargs, bg='#5f7b4d', highlightbackground='#72a84f')
+        return tk.Button(*args, **kwargs, bg='#5a8db8', highlightbackground='#3581c1')
 
     def clear_frame(self):
         for obj in self.winfo_children():
